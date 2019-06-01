3be0d173425442229cc72f0a63d838738447ac05
uranium410 <36751459+uranium410@users.noreply.github.com>
2019-06-01 13:33:16 +0900

    Update DQN_EEP.py

--- DQN_EEP.py
+++ DQN_EEP.py
@@ -7,22 +7,45 @@ import numpy as numpy
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as finished
+import torch.nn.functional as F
+import torchvision.transforms as T
 
 RESULT_DISPLAY_TIMES = 10
-
-EPISODE_NUM = 50
+EPISODE_NUM = 1
 
 env = gym.make('MontezumaRevenge-v0')
 
 total_reward = 0
 
+
+#DQN Network
+#reference: http://torch.classcat.com/2018/05/15/pytorch-tutorial-intermediate-reinforcement-q-learning/
+class DQN(nn.Module):
+ 
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.head = nn.Linear(448, 18)
+ 
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
+
 # Add representative state for first partition
+
+network = DQN()
 
 for episode in range(EPISODE_NUM):
 
     #reset episode
-    env.reset()
+    next_obserbation = env.reset()
     total_reward = 0
 
     while True:
@@ -40,6 +63,11 @@ for episode in range(EPISODE_NUM):
         # DQN()
 
         # EELearning()
+        nnInput = torch.FloatTensor([next_obserbation])
+        print(nnInput.size())
+        #network.forward(nnInput)
+        #print(network.forward(tensorObs))
+
         action = 5
         next_obserbation, reward, done, info = env.step(action)
         #env.render()
