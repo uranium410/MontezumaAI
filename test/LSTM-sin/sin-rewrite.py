4c5de766ffec2be9369913a22947791d9b269d1a
uranium410 <36751459+uranium410@users.noreply.github.com>
2019-07-26 14:53:49 +0900

    20190726

--- test/LSTM-sin/sin-rewrite.py
+++ test/LSTM-sin/sin-rewrite.py
@@ -22,13 +22,26 @@ class Predictor(nn.Module):
 
         return output
 
+def makeRandomBatch(dat1, dat2, batch_size):
+    batch1 = []
+    batch2 = []
+    for _ in range(batch_size):
+        point = random.randint(0, len(dat1)-1)
+        batch1.append(dat1[point])
+        batch2.append(dat2[point])
+
+    return batch1, batch2
+
 Episodes = 100
 Steps = 200
 Data_length=400
 freq = 100
 noise = 0.1
+batch_size = 20
 
 progressDisp = ['-','\\','|','/']
+
+LossFunc = nn.MSELoss()
 
 def main():
     model = Predictor(1,1,1)
@@ -43,16 +56,22 @@ def main():
         predict = []
         losses = []
         randomStart = random.random()*2*math.pi
-
         for i in range(Data_length):
             dat.append(math.sin(2*math.pi*i/freq + randomStart + np.random.normal(loc=0.0, scale=noise)))
             correct.append(math.sin(2*math.pi*(i+1)/freq + randomStart))
 
+        #simulation
         for step in range(Steps):
             inputDat = torch.tensor([[[dat[i]]]])
 
             predict.append(model(inputDat))
-            losses.append(nn.MSELoss(predict[step],correct[step]))
+            #losses.append(LossFunc(predict[step],torch.tensor([[correct[step]]])))            
+
+        batch1, batch2 = makeRandomBatch(predict, correct, batch_size)
+
+        for i in range(len(batch1)-1):
+            loss = LossFunc(batch1[i], torch.tensor(batch2[i]))
+            loss.backward()
 
         print('\r training.',progressDisp[eps % 4] , end="")
 
